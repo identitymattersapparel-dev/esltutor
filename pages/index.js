2aff0a616a6e8c5f21a2216e8b7e5bdbb234c81a
identitymattersapparel-dev <identitymattersapparel@gmail.com>
2026-04-23 10:52:42 -0500

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ maxWidth: 500, margin: '100px auto', textAlign: 'center' }}>
+      <h1>ESL Tutor POC</h1>
+      <p>Home page is working.</p>
+      <a href="/signup">Go to signup</a>
+    </div>
+  )
+}
