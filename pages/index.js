5fc5d52bcad0b1afafe5c790265a2d54b4f77682
identitymattersapparel-dev <identitymattersapparel@gmail.com>
2026-04-23 11:20:57 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,11 @@ export default function Home() {
     <div style={{ maxWidth: 500, margin: '100px auto', textAlign: 'center' }}>
       <h1>ESL Tutor POC</h1>
       <p>Home page is working.</p>
-      <a href="/signup">Go to signup</a>
+
+      <div style={{ display: 'flex', gap: 12, justifyContent: 'center', marginTop: 24 }}>
+        <a href="/signup">Go to signup</a>
+        <a href="/login">Go to login</a>
+      </div>
     </div>
   )
 }
